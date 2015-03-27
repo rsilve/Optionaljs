624cd8574ddaf6ae7bc7c32a6f7c8ea59300d474
Robert Silve <robert@silve.net>
2015-03-27 21:52:22 +0100

    first import

---
+++ src/js/optional.js
@@ -0,0 +1,122 @@
+(function (factory) {
+
+    // Enable multiple loading tool
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory());
+    } else if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        // Node js
+        module.exports = factory();
+    } else {
+
+        window.Optional = factory()
+    }
+
+})(function () {
+
+    // private constructor
+    function Option(val) {
+        var _value = val;
+
+        /**
+         * return wether is empty or not
+         * @returns {boolean}
+         */
+        this.isEmpty = function() {
+            return _value === undefined;
+        };
+
+        /**
+         * return the value if not empty
+         * else throw an error
+         * @returns {*}
+         */
+        this.get = function () {
+            if (this.isEmpty()) {
+                throw new Error('NoSuchElementError');
+            }
+            return _value;
+        };
+
+        /**
+         * transform an option to a new option
+         * by apply parameter function to value if not empty
+         * @param fn {function}
+         * @returns {Option}
+         */
+        this.map = function (fn) {
+            if (this.isEmpty()) {
+                return empty();
+            }
+            if (typeof fn == 'function') {
+                return ofNullable(fn(_value));
+            }
+            return ofNullable(_value[fn](_value));
+        };
+
+        /**
+         * Flatten an Option of Option to simple Option
+         * @returns {Option}
+         */
+        this.flatten = function() {
+            if (_value.isEmpty()) {
+                return empty();
+            }
+            return _value;
+        };
+
+        /**
+         * transform an option a new option
+         * by apply parameter function to value if not empty
+         * and flatten
+         *
+         * @param fn {function}
+         * @returns {Option}
+         */
+        this.flatMap = function(fn) {
+            return this.map(fn).flatten();
+        };
+
+
+        this.getOrElse = function (other) {
+            return this.isEmpty() ? other : _value ;
+        };
+
+        this.toString = function() {
+            return this.isEmpty() ? "Optional.empty()" : "Optional("+_value.toString()+")";
+        };
+
+    }
+
+    /**
+     * return immutable empty option
+     */
+    function empty() {
+        return Object.freeze(new Option());
+    }
+
+    /**
+     * return immutable non empty option
+     */
+    function of(val) {
+        if (val === undefined || val === null) {
+            throw new Error('NoSuchElementError');
+        }
+        return Object.freeze(new Option(val));
+    }
+
+    /**
+     * return immutable option
+     */
+    function ofNullable(val) {
+        var inst = val !== null && val !== undefined  ? of(val) : empty();
+        return Object.freeze(inst);
+    }
+
+    return {
+        empty: empty,
+        of: of,
+        ofNullable: ofNullable
+    }
+});
---
+++ test/js/optional_test.js
@@ -0,0 +1,74 @@
+/**
+ * Created by robert on 27/03/15.
+ */
+
+var should = require('should');
+var Optional = require('../../src/js/optional');
+
+describe('Optional', function(){
+
+    describe('#empty()', function(){
+        it('should return an empty option', function(){
+            Optional.empty().isEmpty().should.be.true;
+            Optional.empty().map(function() {return false}).getOrElse(true).should.be.true;
+        })
+    });
+
+    describe('#of()', function(){
+        it('should return an not empty option', function(){
+            Optional.of(1).isEmpty().should.be.false;
+            Optional.of(1).get().should.be.equal(1);
+            Optional.of(1).map(function() {return true}).getOrElse(false).should.be.true;
+            (function() {Optional.of(null)}).should.be.throw();
+        })
+    });
+
+    describe('#ofNullable()', function(){
+        it('should return an not empty option given a not null value', function(){
+            Optional.ofNullable(1).isEmpty().should.be.false;
+            Optional.ofNullable(1).get().should.be.equal(1);
+            Optional.ofNullable(1).map(function() {return true}).getOrElse(false).should.be.true;
+        })
+        it('should return an  empty option given a null value', function(){
+            Optional.ofNullable(null).isEmpty().should.be.true;
+            Optional.ofNullable(null).map(function() {return false}).getOrElse(true).should.be.true;
+        })
+    });
+
+    describe('#map()', function(){
+        it('should have a map method that transform an option in option', function(){
+            Optional.of(1).map(function(v) {return v+1}).getOrElse(0).should.be.equal(2);
+        })
+    });
+
+    describe('#flatten()', function(){
+        it('should have a flatten method that flatten an option  of option in option', function(){
+            Optional.of(Optional.of(1)).flatten().get().should.be.equal(1);
+            Optional.of(Optional.empty()).flatten().isEmpty().should.be.true;
+            (function() { Optional.of(1).flatten()}).should.throw;
+        })
+    });
+
+    describe('#flatMap()', function(){
+        it('should have a flatMap method that transform an option  of option in option', function(){
+            Optional.of(1).flatMap(function(v) {return Optional.of(v+1)}).getOrElse(0).should.be.equal(2);
+            (function() {Optional.of(1).flatMap(function(v) {return v+1})}).should.throw;
+        })
+    });
+
+    describe('#get()', function(){
+        it('should have a get method that return value of not empty option', function(){
+            Optional.of(1).get().should.be.equal(1);
+            (function() { Optional.empty().get() }).should.throw()
+        })
+    });
+
+    describe('#getOrElse()', function(){
+        it('should have a getOrElse method that return value for empty option', function(){
+            Optional.empty().getOrElse(1).should.be.equal(1);
+            Optional.of(1).getOrElse(2).should.be.equal(1);
+        })
+    });
+
+
+});
